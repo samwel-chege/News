eafc360ecee3c7afe4b65e188e5e3b9e3b6b9666
samwel-chege <samwelchegeh09@gmail.com>
2021-08-07 19:21:34 +0300

    add the message variable

--- app/views.py
+++ app/views.py
@@ -8,4 +8,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    message  = 'Sometimes my genius is almost frightening'
+    return render_template('index.html', message = message)
