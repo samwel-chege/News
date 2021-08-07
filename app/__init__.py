1373b15cd411848cd3e2f2b6f837d6f9b6ce6cf1
samwel-chege <samwelchegeh09@gmail.com>
2021-08-07 19:49:58 +0300

    create a method to set up configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
+
 from flask import Flask
+from .config import DevConfig
 
 #Initializing the application
 app = Flask(__name__)
+
+#Setting up configurations
+app.config.from_object(DevConfig)
+
 from app import views
