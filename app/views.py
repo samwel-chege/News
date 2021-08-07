dde5601a947fb2cd5aea6b6e854b342c88148350
samwel-chege <samwelchegeh09@gmail.com>
2021-08-07 19:27:24 +0300

    add a view function

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+import re
 from flask import render_template
 from app import app 
 
@@ -11,3 +12,11 @@ def index():
 
     message  = 'Sometimes my genius is almost frightening'
     return render_template('index.html', message = message)
+
+@app.route('/news/<int:news_id>') 
+def news(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    ''' 
+    return render_template('news.html',id  = news_id)  
